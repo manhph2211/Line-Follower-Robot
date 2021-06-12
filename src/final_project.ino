23feb992590402648faa538b8f07435db20fc5e7
manhph2211 <maxph22112000@gmail.com>
2021-06-12 19:39:20 +0700

    update motor

--- src/final_project.ino
+++ src/final_project.ino
@@ -1,10 +1,12 @@
-#define AIN1 2 //motor a = +
-#define AIN2 4 //motor a = -
-#define BIN1 7 //motor b = -
-#define BIN2 8 //motor b = +
+#include "motor.h"
 
-#define PWMB 11  //speed motor b
-#define PWMA 10  //speed motor a
+#define AIN1 2
+#define BIN1 7
+#define AIN2 4
+#define BIN2 8
+#define PWMA 5
+#define PWMB 6
+#define STBY 9
 
 #define L1_S A1 
 #define L2_S A0
@@ -12,65 +14,63 @@
 #define R2_S A4
 #define C_S A2
 
+const int offsetA = 1;
+const int offsetB = 1;
+int dem = 0;
 
-void setup() 
+Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
+Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
+
+void setup()
 {
-pinMode(AIN1, OUTPUT);
-pinMode(AIN2, OUTPUT);
-pinMode(BIN1, OUTPUT);
-pinMode(BIN2, OUTPUT);
-pinMode(PWMB, OUTPUT);
-pinMode(PWMA, OUTPUT);
- 
 pinMode(L1_S, INPUT);
 pinMode(L2_S, INPUT);
 pinMode(C_S,  INPUT);
 pinMode(R1_S, INPUT);
 pinMode(R2_S, INPUT);
+}
 
- 
-analogWrite(S_A, 150); 
-analogWrite(S_B, 150); 
-delay(200);
+int check(int a,int b)
+{
+   if(a==1||b==1)
+      return 0;
+   else
+      return 1; 
 }
+
+
 void loop()
-{  
-if ((digitalRead(L_S) == 0)&&(digitalRead(S_S) == 1)&&(digitalRead(R_S) == 0)){forword();}
- 
-if ((digitalRead(L_S) == 1)&&(digitalRead(S_S) == 1)&&(digitalRead(R_S) == 0)){turnLeft();}
-if ((digitalRead(L_S) == 1)&&(digitalRead(S_S) ==0)&&(digitalRead(R_S) == 0)) {turnLeft();}
- 
-if ((digitalRead(L_S) == 0)&&(digitalRead(S_S) == 1)&&(digitalRead(R_S) == 1)){turnRight();}
-if ((digitalRead(L_S) == 0)&&(digitalRead(S_S) == 0)&&(digitalRead(R_S) == 1)){turnRight();}
- 
-if ((digitalRead(L_S) == 1)&&(digitalRead(S_S) == 1)&&(digitalRead(R_S) == 1)){Stop();}
-}
- 
-void forword(){
-digitalWrite(M_A1, LOW);
-digitalWrite(M_A2, HIGH);
-digitalWrite(M_B1, HIGH);
-digitalWrite(M_B2, LOW);  
-}
- 
- 
-void turnRight(){
-digitalWrite(M_A1, LOW);
-digitalWrite(M_A2, LOW);
-digitalWrite(M_B1, HIGH);
-digitalWrite(M_B2, LOW);  
-}
- 
-void turnLeft(){
-digitalWrite(M_A1, LOW);
-digitalWrite(M_A2, HIGH);
-digitalWrite(M_B1, LOW);
-digitalWrite(M_B2, LOW);
-}
- 
-void Stop(){
-digitalWrite(M_A1, LOW);
-digitalWrite(M_A2, LOW);
-digitalWrite(M_B1, LOW);
-digitalWrite(M_B2, LOW);
+{
+
+   if ( check(digitalRead(L1_S),digitalRead(L2_S)) && check(digitalRead(R1_S),digitalRead(R2_S)) && digitalRead(L2_S) == 1)
+   {
+   forward(motor1, motor2, 150);
+   delay(1000);
+   }
+
+   //back(motor1, motor2, -150);
+   //delay(1000);
+   
+   brake(motor1, motor2);
+   delay(1000);
+   
+   if (!check(digitalRead(L1_S),digitalRead(L2_S)) && check(digitalRead(R1_S),digitalRead(R2_S)) && digitalRead(L2_S) == 1)
+   {
+   left(motor1, motor2, 100);
+   delay(1000);
+   }
+
+   if (check(digitalRead(L1_S),digitalRead(L2_S)) && !check(digitalRead(R1_S),digitalRead(R2_S)) && digitalRead(L2_S) == 0)
+   {
+   right(motor1, motor2, 100);
+   delay(1000);
+   }
+
+   if (!check(digitalRead(L1_S),digitalRead(L2_S)) && !check(digitalRead(R1_S),digitalRead(R2_S)) && digitalRead(L2_S) == 0)
+   {
+   dem += 1;
+   delay(1000); 
+   }
+
+   
 }
