44a3ae109bb84c048881c95a0f1cba786cb3d984
manhph2211 <maxph22112000@gmail.com>
2021-06-23 10:14:26 +0700

    update timer

--- final_project/final_project.ino
+++ final_project/final_project.ino
@@ -21,76 +21,76 @@
 
 const int offsetA = 1;
 const int offsetB = 1;
-int dem = 0;
-SimpleTimer timer;
-char auth[] = ""; 
 
-// Your WiFi credentials.
-// Set password to "" for open networks.
+int count = 0;
+float speed = 0;
+
+SimpleTimer timer;
+
+char auth[] = ""; 
 char ssid[] = "";  //Enter your WIFI Name
 char pass[] = "";  //Enter your WIFI Password
 
 Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
 Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
 
-void setup()
-{
-pinMode(L1_S, INPUT);
-pinMode(L2_S, INPUT);
-pinMode(C_S,  INPUT);
-pinMode(R1_S, INPUT);
-pinMode(R2_S, INPUT);
-Serial.begin(9600);
-Blynk.begin(auth, ssid, pass);
 
+BLYNK_WRITE(V1)
+{
+	speed = param.asFloat(); 
 }
 
 
 void processor()
 {
-  int s1 = digitalRead(L1_S);  //Left Most Sensor
-  int s2 = digitalRead(L2_S);  //Left Sensor
-  int s3 = digitalRead(C_S);  //Middle Sensor
-  int s4 = digitalRead(R1_S);  //Right Sensor
-  int s5 = digitalRead(R2_S);  //Right Most Sensor
+    int s1 = digitalRead(L1_S);  //Left Most Sensor
+    int s2 = digitalRead(L2_S);  //Left Sensor
+    int s3 = digitalRead(C_S);  //Middle Sensor
+    int s4 = digitalRead(R1_S);  //Right Sensor
+    int s5 = digitalRead(R2_S);  //Right Most Sensor
   
-   if((s1 == 1) && (s2 == 1) && (s3 == 0) && (s4 == 1) && (s5 == 1))
-   {
-   forward(motor1, motor2, 150);
-   delay(1000);
-   }
+    if((s1 == 0) && (s2 == 0) && (s3 == 1) && (s4 == 0) && (s5 == 0))
+    {
+    Serial.println ("Moving Forward...");
+    forward(motor1, motor2, speed);
+    }
+      
+    else if ((s1 == 1) || (s2 == 1) && (s4 == 0) && (s5 == 0))
+    {
+    Serial.println ("Turing Left...");
+    left(motor1, motor2, speed);
+    }
 
-   back(motor1, motor2, -150);
-   delay(1000);
-   
-   brake(motor1, motor2);
-   delay(1000);
-   
-   if (!check(analogRead(L1_S),analogRead(L2_S)) && check(analogRead(R1_S),analogRead(R2_S)) && convert(analogRead(C_S)) == 1)
-   {
-   left(motor1, motor2, 100);
-   delay(1000);
-   }
+    esle if ((s1 == 0) && (s2 == 0) && ((s4 == 1) &|| (s5 == 1)))
+    {
+    Serial.println ("Turning right...");
+    right(motor1, motor2, speed);
+    }
 
-   if (check(analogRead(L1_S),analogRead(L2_S)) && !check(analogRead(R1_S),analogRead(R2_S)) && convert(analogRead(C_S)) == 0)
-   {
-   right(motor1, motor2, 100);
-   delay(1000);
-   }
-
-   if (!check(analogRead(L1_S),analogRead(L2_S)) && !check(analogRead(R1_S),analogRead(R2_S)) && convert(analogRead(C_S)) == 0)
-   {
-   dem += 1;
-   delay(1000); 
-   }
-
-   
+    else if (((s1 == 1) || (s2 == 1)) && (s3 == 1) && ((s4 == 1) || (s5 == 1)))
+    {
+    Serial.println ("Stop...");
+    count += 1;
+    brake(motor1, motor2);  
+    }   
 }
+
+
+void setup()
+{
+	pinMode(L1_S, INPUT);
+	pinMode(L2_S, INPUT);
+	pinMode(C_S,  INPUT);
+	pinMode(R1_S, INPUT);
+	pinMode(R2_S, INPUT);
+	Serial.begin(9600);
+	Blynk.begin(auth, ssid, pass);
+	timer.setInterval(1000L, processor);
+}
+
 
 void loop()
 {
-
-  Blynk.run(); // Initiates Blynk
-  timer.run(); // Initiates SimpleTimer
-  
+	Blynk.run(); 
+	timer.run();
 }
