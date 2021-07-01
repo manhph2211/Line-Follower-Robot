04c1c096ba5caa351470ea70bc6197ddf45b8500
manhph2211 <maxph22112000@gmail.com>
2021-07-01 22:31:14 +0700

    update

--- main/SimpleTimer.cpp
+++ main/SimpleTimer.cpp
@@ -22,6 +22,55 @@
  * write to the Free Software Foundation, Inc.,
  * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
+///
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #include "SimpleTimer.h"
